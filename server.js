87c8e9bd86822c64bccc034d0417cff387dcb060
Rogerio <rogerio@universidadeglobal.com.br>
2025-05-18 11:09:32 -0300

    commit

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+const variavel = process.env.OTIMIZADO;
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, `${variavel}`));
 });
+
+console.log(`Variável de ambiente OTIMIZADO: ${variavel}`);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
